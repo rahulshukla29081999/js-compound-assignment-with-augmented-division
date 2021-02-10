c9137706b43261429dea6e15076c658e6462fd87
rahulshukla29081999 <shuklarahulkanpur@gmail.com>
2021-02-10 23:09:03 +0530

    initial

---
+++ compound-assignment-with-augmented-division.js
@@ -0,0 +1,13 @@
+
+//Compound Assignment With Augmented Division.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+console.log(a);
+console.log(b);
+console.log(c);
